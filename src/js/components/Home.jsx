fbb7ad6b39bfdded3a584feafb1fc10804ed0dcc
zoscra <jmrg.trabajo@gmail.com>
2025-06-16 10:54:04 +0000

    Todolist

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,62 +1,91 @@
-import React, { useState } from "react";
+import React, { useState,useEffect } from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
+
 //create your first component
 const Home = () => {
 	const [tarea,setTarea] = useState("");
-	const [listaTarea,setListaTarea]=useState([]);
+	const [state,setState] = useState([])
+	
+const getTodos = async() => {
+    try {
+        const result =await fetch("https://playground.4geeks.com/todo/users/todo1")
+        const data =await result.json();
+		console.log(data)
+        setState(data.todos)
+    }catch(error){
+        console.log("error 257",error)
+    }
+}
 
-	function handleTareasAñadir (event){
-		event.preventDefault();
-		let tareaActual = tarea;
-		setListaTarea(prev=>[...prev,tareaActual])
+const postTodos = async () => {
+	try {
+		await fetch('https://playground.4geeks.com/todo/todos/todo1',{
+		method: "POST",
+		headers: {"Content-Type":"application/json"},
+		body: JSON.stringify({
+			"label": tarea,
+			"is_done": false
+		})
+	})
+	setTarea("");
+	await getTodos();
+	} catch (error) {
+		console.log('error 404:',error)
 	}
+	
+}
+	
+const deleteTodos = async(id)=>{
+	await fetch(`https://playground.4geeks.com/todo/todos/${id}` ,{
+		method: "DELETE",
+	})
 
-	function handleTareasBorrar (indexItem){
-		setListaTarea(prev=>prev.filter((prev,index)=>index!==indexItem));
+}
 
-	}
+useEffect(()=>{
+getTodos()
+},[])
 
 
 	return (
 		<div className="text-center m-5">
-			<form class="mb-3" onSubmit={(e)=>handleTareasAñadir(e)}>
+			<form className="mb-3" onSubmit={(e)=>{e.preventDefault(); postTodos()}}>
 				<label 
-				for="tareas" 
-				class="form-label"
+				htmlFor="tareas" 
+				className="form-label"
 				
 				>Tareas</label>
 				<input 
 				type="text" 
-				class="form-control" 
+				className="form-control" 
 				id="tareas" 
 				placeholder="Sacar la basura"
 				value={tarea}
 				onChange={(e)=>setTarea(e.target.value)}
 				
 				/>
-			<div class="col-auto">
+			<div className="col-auto">
     			<button 
 				type="submit" 
-				class="btn btn-primary mb-3"
+				className="btn btn-primary mb-3"
 				>Confirmar Tarea</button>
   			</div>			
 				</form>
 				
 				{
-					listaTarea.map((item,index)=>{
+					state.map((item)=>{
 						return(
-							<div className="mb-3">
-								<span>{item}</span>
-								<button className="btn btn-primary" onClick={()=>handleTareasBorrar(index)}>Borrar tarea</button>
+							<div className="mb-3" key={item.id}>
+								<span>{item.label}</span>
+								<button className="btn btn-primary" onClick={()=>{deleteTodos(item.id); getTodos()}}>Borrar tarea</button>
 							</div>
 						)
 					}
 				)
 				}
-			
 		</div>
 	);
 };
